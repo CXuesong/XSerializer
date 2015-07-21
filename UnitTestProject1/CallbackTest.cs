b6e5459c7eeab680e323a93e5ed21956e3e56a2c
forest93 <forest93@163.com>
2015-07-21 18:52:03 +0800

    补充提交。

---
+++ UnitTestProject1/CallbackTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.Serialization;
+using System.Xml.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Undefined.Serialization;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class CallbackTest
+    {
+        public class MyClass
+        {
+            [XElement]
+            public string myString;
+
+            [OnSerializing]
+            private void OnSerializing(StreamingContext context)
+            {
+                Trace.WriteLine(string.Format("OnSerializing, {0}, {1}", context.State, context.Context));
+                Trace.WriteLine(string.Format("\tmyString = {0}", myString));
+            }
+
+            [OnSerialized]
+            private void OnSerialized(StreamingContext context)
+            {
+                Trace.WriteLine(string.Format("OnSerialized, {0}, {1}", context.State, context.Context));
+            }
+
+            [OnDeserializing]
+            private void OnDeserializing(StreamingContext context)
+            {
+                Trace.WriteLine(string.Format("OnSerializing, {0}, {1}", context.State, context.Context));
+            }
+
+            [OnDeserialized]
+            private void OnDeserialized(StreamingContext context)
+            {
+                Trace.WriteLine(string.Format("OnDeserialized, {0}, {1}", context.State, context.Context));
+                Trace.WriteLine(string.Format("\tmyString = {0}", myString));
+            }
+
+            public MyClass()
+            {
+
+            }
+
+            public MyClass(string str)
+            {
+                myString = str;
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var s = new XSerializer(typeof(MyClass));
+            var obj = new MyClass("Callback Test");
+            var doc = s.GetSerializedDocument(obj);
+            Trace.WriteLine(doc);
+            var obj1 = (MyClass)s.Deserialize(doc, null);
+            Assert.AreEqual(obj.myString, obj1.myString);
+        }
+    }
+}
